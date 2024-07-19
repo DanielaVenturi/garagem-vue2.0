4b746cc96e42fa6bc72517cd0f2821a075d5f2ed
DanielaVenturi <danielaventuri142gmail.com>
2024-07-19 16:55:49 -0300

    atualização do loginView

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -5,7 +5,7 @@ import "@passageidentity/passage-elements/passage-auth";
 <template>
   <h1>Login</h1>
   <div class="authContainer">
-    <passage-auth app-id="oFRpSGKxXdn9hMSgmQL8cVMh"></passage-auth>
+    <passage-auth app-id="WvX1ea7it8pgVBPSYR3dD8ui"></passage-auth>
   </div>
 </template>
 
